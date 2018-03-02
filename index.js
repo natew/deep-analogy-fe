308f2a99c5fe46c90b0eb630697688f8f59caaf8
natew <natewienert@gmail.com>
2018-03-02 14:11:41 -0800

    settings

--- index.js
+++ index.js
@@ -30,45 +30,66 @@ app.use(
 )
 
 // routes
-app.get('/', function(req, res) {
-  res.send(
-    '<html><head><title>Dropzone example</title><link href="/dropzone/downloads/css/dropzone.css" rel="stylesheet"></head><body><h1>Using Dropzone</h1><form method="post" action="/" class="dropzone" id="dropzone-example"><div class="fallback"><input name="file" type="file" multiple /></div></form><p><a href="/old">Old form version</a></p><script src="/dropzone/downloads/dropzone.js"></script></body></html>',
-  )
-})
+app.get('/', express.static('templates'))
 
 let files = []
+let settings = '1 3 0'
+let SETTING_VALS = {
+  0: '1 3',
+  1: '0.5 3',
+  2: '0 2',
+  3: '0.5 2',
+  4: '1 1',
+}
 
 app.post('/', function(req, res) {
-  var file = req.files.file
-  files.push(file)
+  if (req.body && req.body.type) {
+    console.log('settings', req.body)
+    let type = req.body.type
+    let smoothing = req.body.smoothing
+    if (SETTING_VALS[type]) {
+      settings = `${SETTING_VALS[type]} ${smoothing === 'on' ? 1 : 0}`
+    }
+    res.redirect('/')
+    return
+  }
 
-  if (files.length === 2) {
-    let current = files
-    files = [] // reset for next run
-    let allResults = fs.readdirSync(RESULTS_DIR)
-    if (!allResults) return
-    let out = path.join(RESULTS_DIR, `out_${allResults.length + 1}`)
-    let content = path.join(__dirname, current[0].path)
-    let style = path.join(__dirname, current[1].path)
-    execa.shellSync(`mkdir ${out}`)
-    execa.shellSync(
-      `./demo deep_image_analogy/models/ ${content} ${style} ${out} 0 0.5 3 0`,
-      {
-        cwd: DEEP_ANALOGY_DIR,
-        env: {
-          LD_LIBRARY_PATH:
-            '/home/nw/deep-analogy/build/lib:/usr/local/cuda/lib64',
+  var file = req.files.file
+  if (file) {
+    files.push(file)
+
+    if (files.length === 2) {
+      let current = files
+      files = [] // reset for next run
+      let allResults = fs.readdirSync(RESULTS_DIR)
+      if (!allResults) return
+      let out = path.join(RESULTS_DIR, `out_${allResults.length + 1}`)
+      let content = path.join(__dirname, current[0].path)
+      let style = path.join(__dirname, current[1].path)
+      execa.shellSync(`mkdir ${out}`)
+      execa.shellSync(
+        `./demo deep_image_analogy/models/ ${content} ${style} ${out} 0 ${settings}`,
+        {
+          cwd: DEEP_ANALOGY_DIR,
+          env: {
+            LD_LIBRARY_PATH:
+              '/home/nw/deep-analogy/build/lib:/usr/local/cuda/lib64',
+          },
         },
-      },
-    )
-    console.log('done!')
+      )
+      console.log('done!')
+    }
   }
 
   res.sendStatus(200)
 })
 
-var server = app.listen(80, '0.0.0.0', function() {
-  var host = server.address().address,
-    port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
-})
+var server = app.listen(
+  process.env.NODE_ENV === 'development' ? 3000 : 80,
+  '0.0.0.0',
+  function() {
+    var host = server.address().address,
+      port = server.address().port
+    console.log('Example app listening at http://%s:%s', host, port)
+  },
+)
